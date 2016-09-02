a568dea4b328df9dc98e787ae58fced27b3aba77
Robert Wlodarczyk <rwlodarc@impinj.com>
2016-09-02 11:19:45 -0700

    Missing ,

--- setup.py
+++ setup.py
@@ -25,6 +25,10 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: System :: Hardware',
         'Topic :: System :: Networking',
     ],
@@ -53,7 +57,7 @@ setup(
     tests_require=[
         'pylint',
         'flake8',
-        'flake8-docstrings'
+        'flake8-docstrings',
         'nose',
         'nosexcover',
     ],
